9f391ae262dd255321d328320f667c59170bc7cc
rajendraprasad-hub <rp577514@gmail.com>
2026-02-25 00:15:35 +0530

    Update k8s-post-setup.sh

--- k8s-post-setup.sh
+++ k8s-post-setup.sh
@@ -10,4 +10,3 @@ echo 'source <(kubectl completion bash)' >> ~/.bashrc
 source ~/.bashrc
 
 echo "===== Cluster Setup Completed ====="
-kubectl get nodes  echo "===== Master Node Configuration  Completed ====="
